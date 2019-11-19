7971af363ceb6f1ba7f2f120ef9accc6ee8f07ad
Nebulis <nebounet@gmail.com>
2019-11-19 13:43:51 +0800

    fix: print watermark

--- src/templates/govtechDemoCert/common/PrintWatermark.tsx
+++ src/templates/govtechDemoCert/common/PrintWatermark.tsx
@@ -4,13 +4,19 @@ import { css } from "@emotion/core";
 export const PrintWatermark: FunctionComponent = () => (
   <div
     css={css`
+      width: 0;
+      height: 0;
+      opacity: 0;
+      display: none;
+      position: absolute;
+      background-image: url("/static/images/watermark.svg");
+      background-repeat: repeat;
+
       @media print {
-        position: absolute;
-        opacity: 0.5;
         width: 100%;
         height: 100%;
-        background-image: url("/images/watermark.svg");
-        background-repeat: repeat;
+        opacity: 0.6;
+        display: block;
       }
     `}
     className="print-only"
