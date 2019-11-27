481fc47f351f42a16bec4c81b9e5874b47c249af
Nebulis <nebounet@gmail.com>
2019-11-27 13:02:32 +0800

    fix: use import for svg logo

--- src/templates/govtechDemoCert/certificate.tsx
+++ src/templates/govtechDemoCert/certificate.tsx
@@ -7,6 +7,7 @@ import "./common/demoStyles.css";
 import { TemplateProps } from "@govtechsg/decentralized-renderer-react-components";
 import { GovtechOpencertsTemplateCertificate } from "../samples";
 import { PrintWatermark } from "./common/PrintWatermark";
+import logo from "./common/opencertslogo.svg";
 
 export const CertificateTemplate: FunctionComponent<TemplateProps<GovtechOpencertsTemplateCertificate>> = ({
   document
@@ -29,11 +30,7 @@ export const CertificateTemplate: FunctionComponent<TemplateProps<GovtechOpencer
         }}
       >
         <div className="my-5 m-lg-5 text-center">
-          <img
-            src={require("./common/opencertslogo.svg")}
-            alt="OpenCerts Logo"
-            style={{ width: "60%", height: "auto", maxWidth: "1000px" }}
-          />
+          <img src={logo} alt="OpenCerts Logo" style={{ width: "60%", height: "auto", maxWidth: "1000px" }} />
         </div>
         <div className="mb-4 mb-lg-5 d-flex justify-content-center cert-body" style={{ textAlign: "center" }}>
           <i>This is to certify that</i>
